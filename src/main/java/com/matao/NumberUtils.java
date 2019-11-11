954b0d8ffc128cfc2b70c0cec3580c980c1a0c17
matao-git <2237362443@qq.com>
2019-11-11 13:14:04 +0800

    自定义异常+日期+文件+数字+流+字符串工具类

---
+++ src/main/java/com/matao/NumberUtils.java
@@ -0,0 +1,15 @@
+package com.matao;
+
+public class NumberUtils {
+	
+	/**
+	 *  求百分比的整数部分的字符�?
+	 * @param n
+	 * @param m
+	 * @return
+	 */
+	public static String percent(int n ,int m) {
+		return 100*n/m + "%";
+	}
+
+}
